6c6abf7bc832e2ac8c44f36ac4e124bb1d970914
ElPerer <pedromorales_96@hotmail.com>
2023-04-21 21:22:51 -0600

    nuevos archivos

--- 01-typescript-intro/src/bases/01-types.ts
+++
@@ -1,9 +0,0 @@
-export let name: string | boolean = 'Pedro';
-export const age = 35;
-
-name = true;
-
-export const templateString = `String
-multilinea
-valor inyectado: ${ name }
-valor $ ${ age }`;
